c4621b267bbef4ecb24b592727005209ba488ce5
trojvn <mrx.trojan@gmail.com>
2024-11-25 00:06:31 +0300

    without ios and light

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { miniApp, useLaunchParams, useSignal } from "@telegram-apps/sdk-react";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
 import { AppRoot } from "@telegram-apps/telegram-ui";
 import { type PropsWithChildren, useEffect } from "react";
 
@@ -14,7 +14,6 @@ import { ErrorPage } from "../ErrorPage";
 function RootInner({ children }: PropsWithChildren) {
   const isDev = process.env.NODE_ENV === "development";
 
-  // Mock Telegram environment in development mode if needed.
   if (isDev) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useTelegramMock();
@@ -23,40 +22,23 @@ function RootInner({ children }: PropsWithChildren) {
   const lp = useLaunchParams();
   const debug = isDev || lp.startParam === "debug";
 
-  // Initialize the library.
   useClientOnce(() => {
     initSdk(debug);
   });
 
-  const isDark = useSignal(miniApp.isDark);
-  //   const initDataUser = useSignal(initData.user);
-
-  // Set the user locale.
-  //   useEffect(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  //     initDataUser && setLocale(initDataUser.languageCode);
-  //   }, [initDataUser]);
-
-  // Enable debug mode to see all the methods sent and events received.
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     debug && import("eruda").then((lib) => lib.default.init());
   }, [debug]);
 
   return (
-    <AppRoot
-      appearance={isDark ? "dark" : "light"}
-      platform={["macos", "ios"].includes(lp.platform) ? "ios" : "base"}
-    >
+    <AppRoot appearance="dark" platform="base">
       {children}
     </AppRoot>
   );
 }
 
 export function Root(props: PropsWithChildren) {
-  // Unfortunately, Telegram Mini Apps does not allow us to use all features of
-  // the Server Side Rendering. That's why we are showing loader on the server
-  // side.
   const didMount = useDidMount();
 
   return didMount ? (
